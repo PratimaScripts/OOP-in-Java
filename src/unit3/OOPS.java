b631a7f7da23e72f53f92d3139c1a06651fa54da
PratimaScripts <pratimagautam.np@gmail.com>
2025-02-27 20:24:04 +0545

    Instantiating a class

---
+++ src/unit3/OOPS.java
@@ -0,0 +1,17 @@
+package unit3;
+
+public class OOPS {
+	public static void main(String[] args) {
+		Student student1 = new Student();
+		student1.name = "Pratima";
+		student1.address = "Naxal, kathmandu";
+		student1.phoneNumber = 985675 ;
+		student1.rollNo = "1";
+		student1.getInfo();
+		
+		Student student2 = new Student();
+		student2.name = "Sandhya Sharma";
+		student2.rollNo = "2";
+		student2.getInfo();	
+	}
+}
