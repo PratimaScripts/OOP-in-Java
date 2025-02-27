e4f87f2c914ef49da382b0d3ee7fae311d790444
PratimaScripts <pratimagautam.np@gmail.com>
2025-02-27 20:31:27 +0545

    Explicit Type Conversion (Type Casting)

---
+++ src/unit3/DoubleToFloat.java
@@ -0,0 +1,11 @@
+package unit3;
+
+public class DoubleToFloat {
+	public static void main(String[] args) {
+		double db= 2222;
+
+		float flt = (float)db;
+
+		System.out.println("Conversion from double to float "+ flt);
+	}
+}
