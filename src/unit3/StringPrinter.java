a9ea2f67dd2a6723c2f3265801d545759e996eb4
PratimaScripts <pratimagautam.np@gmail.com>
2025-03-27 07:22:47 +0545

    Updates on RandomNumberException and StringPrinter

---
+++ src/unit3/StringPrinter.java
@@ -0,0 +1,14 @@
+package unit3;
+
+public class StringPrinter {
+    public static void main(String args[]) {
+        // Create a new String object with the value "Hello World"
+        String quote = "Hello World";
+        
+        // Reassign the quote variable to a new String object with the value "kmg"
+        quote = "kmg";
+        
+        // Print the current value of the quote variable, which is now "kmg"
+        System.out.println("mutable==" + quote);
+    }
+}
