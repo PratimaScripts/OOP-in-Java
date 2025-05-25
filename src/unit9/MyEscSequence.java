3c1f99ce88705a3ff0133dda82350d7029f6da14
Pratima Gautam <pratimagautam.np@gmail.com>
2025-05-25 14:09:31 +0545

    Sample Codes - Escape sequence, Wrapper classes, Math class, Character Methods

---
+++ src/unit9/MyEscSequence.java
@@ -0,0 +1,9 @@
+package unit9;
+
+public class MyEscSequence
+{
+    public static void main(String [] args)
+    {
+        System.out.println("Nepal voted \nand\" the result was as expected!! ");
+    }
+}
