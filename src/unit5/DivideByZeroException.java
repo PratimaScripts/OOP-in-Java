5f44555b9eb9e2c06c4e307cf557a4fcf32c4fae
Pratima Gautam <pratimagautam.np@gmail.com>
2025-05-25 15:11:04 +0545

    Handling exception with Try, Catch and Finally

--- src/unit5/DivideByZeroException.java
+++ src/unit5/DivideByZeroException.java
@@ -6,11 +6,13 @@ public class DivideByZeroException {
 			int i = 10;
 			int b = i / 0;
 			System.out.println("****Divide by zero exception**** " + b);
-		} catch (ArithmeticException e) {
-			// e.printStackTrace();
-			System.out.println("*****ArithmeticException**** " + e);
-		} catch (Exception e) {
-			// e.printStackTrace();
+		} 
+//		catch (ArithmeticException e) {
+//			// e.printStackTrace();
+//			System.out.println("*****ArithmeticException**** " + e);
+//		} 
+		catch (Exception e) {
+//			 e.printStackTrace();
 			System.out.println("In exception Super class of Exception " + e);
 		}
 		System.out.println("****After Two Catch Statement*****");
--- src/unit5/MultipleCatchException.java
+++ src/unit5/MultipleCatchException.java
@@ -6,16 +6,20 @@ public class MultipleCatchException {
 			int a = args.length;
 
 			System.out.println("**** Args values Are **** " + a);
-			int b = 42 / a;
+//			int b = 42 / a;
 			int c[] = {1};
 			c[42] = 99;
-		} catch (ArithmeticException e) {
-			// e.printStackTrace();
+		} 
+		catch (ArithmeticException e) {
+			 e.printStackTrace();
 			System.out.println("***** Divide By ZERO **** " + e);
-		} catch (ArrayIndexOutOfBoundsException e) {
-			// e.printStackTrace();
-			System.out.println("In ArrayIndexOutOfBoundsException " + e);
+		} 
+		catch (ArrayIndexOutOfBoundsException abc) {
+//			 abc.printStackTrace();
+			System.out.println("In ArrayIndexOutOfBoundsException " + abc);
 		}
-		// System.out.println("**** After Try Catch Statement *****");
+		finally {
+			System.out.println("**** After Try Catch Statement *****");
+		} 
 	}
 }
