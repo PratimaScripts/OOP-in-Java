b38ac748302e22241d7b72a57dddc737dd0bcfe8
Pratima Gautam <pratimagautam.np@gmail.com>
2025-05-25 14:00:14 +0545

    Sample Codes on Inheritance

---
+++ src/unit4/MyInheritanceChild.java
@@ -0,0 +1,19 @@
+package unit4;
+
+public class MyInheritanceChild extends MyInheritance
+{
+    // instance variables - replace the example below with your own
+    private int result;
+    
+    public MyInheritanceChild(String n)
+    {
+        super(n);
+    }
+    
+    public void multiplication(int x, int y)
+    {
+      result = x * y;
+      System.out.println("The multiplication is: "+result);
+    }
+    
+}
