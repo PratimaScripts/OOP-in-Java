b542c2abeef29512896ebe6c22e655d03a5632c6
Pratima Gautam <pratimagautam.np@gmail.com>
2025-05-26 23:13:42 +0545

    Added comments

--- src/unit13/SaveRecords.java
+++ src/unit13/SaveRecords.java
@@ -7,7 +7,7 @@ import java.sql.*;
 public class SaveRecords {
 
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/megacollege";
+        String url = "jdbc:mysql://localhost:3306/megacollege"; //orchid
         String uname = "root";
         String pwd = "";
 
